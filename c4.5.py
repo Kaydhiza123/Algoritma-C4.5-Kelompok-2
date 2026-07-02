99d2e832e557d2b4c7b9e05bbb42797f065829df
Kaydhiza Shari Amalya <kaylanovyan@gmail.com>
2026-07-02 08:36:19 +0700

    Update c4.5.py

--- c4.5.py
+++ c4.5.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.preprocessing import LabelEncoder
 
 st.set_page_config(page_title="C4.5 Graduation Predictor", layout="wide")
